7a864f4c2ee15a5be011ceadcb780f16b21da05c
Mateo Díaz <mateo.diaz@epn.edu.ec>
2023-09-14 21:12:33 -0500

    feat: motorcycle info, activity, layout

--- app/src/main/java/com/example/project_gearup/models/Motorcycle.kt
+++ app/src/main/java/com/example/project_gearup/models/Motorcycle.kt
@@ -1,23 +1,68 @@
 package com.example.project_gearup.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 class Motorcycle(
-    var image: Int,
-    var model: String,
+    var image: String?,
+    var model: String?,
     var price: Double,
     var cylinderCapacity: Int,
-    var motor: String,
+    var motor: String?,
     var motorcycleClass: ArrayList<String>,
-    var transmission: String,
-    var suspension: String,
-    var brakes: String,
+    var transmission: String?,
+    var suspension: String?,
+    var brakes: String?,
     var fuelCapacity: Double,
-    var description: String
-) {
+    var description: String?
+): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readDouble(),
+        parcel.readInt(),
+        parcel.readString(),
+        TODO("motorcycleClass"),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readDouble(),
+        parcel.readString()
+    ) {
+    }
+
     fun getMotorcycleClasses(): String {
         var motorcycleClasses = ""
         for (motorcycleClass in this.motorcycleClass) {
             motorcycleClasses += "$motorcycleClass |"
         }
         return motorcycleClasses.dropLast(2)
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(image)
+        parcel.writeString(model)
+        parcel.writeDouble(price)
+        parcel.writeInt(cylinderCapacity)
+        parcel.writeString(motor)
+        parcel.writeString(transmission)
+        parcel.writeString(suspension)
+        parcel.writeString(brakes)
+        parcel.writeDouble(fuelCapacity)
+        parcel.writeString(description)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Motorcycle> {
+        override fun createFromParcel(parcel: Parcel): Motorcycle {
+            return Motorcycle(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Motorcycle?> {
+            return arrayOfNulls(size)
+        }
     }
 }
