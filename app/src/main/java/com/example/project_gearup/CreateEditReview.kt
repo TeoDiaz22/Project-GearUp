01068908999eaff778c30407e7c8aea754a50c2e
Bryan Tapia <bryan.tapia03@epn.edu.ec>
2023-09-14 21:15:46 -0500

    feat: add createEdit

---
+++ app/src/main/java/com/example/project_gearup/CreateEditReview.kt
@@ -0,0 +1,11 @@
+package com.example.project_gearup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateEditReview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_edit_review)
+    }
+}
